9d5d5f7f98151dda135eb9f58ca51989ed53dae3
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-11-01 21:47:05 -0300

    Requisito 2 - feito

---
+++ src/models/User.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      field: 'display_name'
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING
+    },
+  }, 
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: "users"
+  });
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      as:'posts',
+      foreignKey: 'user_id'
+    })
+  }
+  return User;
+};
